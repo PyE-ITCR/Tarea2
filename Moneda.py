b2caf29aa402689351c3d687cc73c9d4b432d109
Agustín V <joseagustinveneveg@gmail.com>
2021-09-07 14:23:01 -0600

    [ADD] Planteamiento

---
+++ Moneda.py
@@ -0,0 +1,7 @@
+#Para el primer caso se selccionó el método de distribución VAD: Distribución Gométrica. Esto debido a que el mismo se centra en el objetivo
+#Planteado, que es el número de ensayos que se deben llevar a cabo antes de conseguir un único éxito, siendo este que salga un escudo.
+
+#Para el segundo caso se seleccionó el método de distribución VAD: Distribución Binominal Negativa, debido a los parámetros y descripción del
+#mismo, pues el mismo encuentra la cantidad de ensayos que se deben realizar para conseguir N éxitos, que es el problema planteado.
+
+#Una vez averiguada la cantidad de ensayos por éxito se debe construir la funcion de masa de probabilidad y graficar
